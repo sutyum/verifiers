9fa0f7b1a818d2f920d892cd5f8855a835167d27
Satyam Tiwary <satyam@technoculture.io>
2025-03-09 22:42:18 +0530

    more minor

--- verifiers/__init__.py
+++ verifiers/__init__.py
@@ -6,31 +6,31 @@
 # from verifiers.envs.tool_env import ToolEnv
 # from verifiers.trainers.grpo_env_trainer import GRPOEnvTrainer
 # from verifiers.utils.data_utils import extract_boxed_answer, extract_hash_answer, preprocess_dataset
-# from verifiers.utils.model_utils import get_model, get_tokenizer, get_model_and_tokenizer
+from verifiers.utils.model_utils import get_model, get_tokenizer, get_model_and_tokenizer
 # from verifiers.utils.config_utils import get_default_grpo_config
-# from verifiers.utils.logging_utils import setup_logging, print_prompt_completions_sample
-#
-#
-# __version__ = "0.1.0"
-#
-# # Setup default logging configuration
-# setup_logging()
-#
-# __all__ = [
-#     "Environment",
-#     "CodeEnv",
-#     "DoubleCheckEnv",
-#     "MathEnv",
-#     "SimpleEnv",
-#     "ToolEnv",
-#     "GRPOEnvTrainer",
-#     "get_model",
-#     "get_tokenizer",
-#     "get_model_and_tokenizer",
-#     "get_default_grpo_config",
-#     "extract_boxed_answer",
-#     "extract_hash_answer",
-#     "preprocess_dataset",
-#     "setup_logging",
-#     "print_prompt_completions_sample",
-# ]
+from verifiers.utils.logging_utils import setup_logging, print_prompt_completions_sample
+
+
+__version__ = "0.1.0"
+
+# Setup default logging configuration
+setup_logging()
+
+__all__ = [
+    # "Environment",
+    # "CodeEnv",
+    # "DoubleCheckEnv",
+    # "MathEnv",
+    # "SimpleEnv",
+    # "ToolEnv",
+    # "GRPOEnvTrainer",
+    "get_model",
+    "get_tokenizer",
+    "get_model_and_tokenizer",
+    # "get_default_grpo_config",
+    # "extract_boxed_answer",
+    # "extract_hash_answer",
+    # "preprocess_dataset",
+    # "setup_logging",
+    # "print_prompt_completions_sample",
+]
