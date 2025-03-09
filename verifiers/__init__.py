ebc463410a85438692ad80fac0de557f087dc1d6
Satyam Tiwary <satyam@technoculture.io>
2025-03-09 22:57:51 +0530

    more minor

--- verifiers/__init__.py
+++ verifiers/__init__.py
@@ -1,13 +1,13 @@
-# from verifiers.envs.environment import Environment
-# from verifiers.envs.code_env import CodeEnv
-# from verifiers.envs.doublecheck_env import DoubleCheckEnv
-# from verifiers.envs.math_env import MathEnv
-# from verifiers.envs.simple_env import SimpleEnv
-# from verifiers.envs.tool_env import ToolEnv
-# from verifiers.trainers.grpo_env_trainer import GRPOEnvTrainer
-# from verifiers.utils.data_utils import extract_boxed_answer, extract_hash_answer, preprocess_dataset
+from verifiers.envs.environment import Environment
+from verifiers.envs.code_env import CodeEnv
+from verifiers.envs.doublecheck_env import DoubleCheckEnv
+from verifiers.envs.math_env import MathEnv
+from verifiers.envs.simple_env import SimpleEnv
+from verifiers.envs.tool_env import ToolEnv
+from verifiers.trainers.grpo_env_trainer import GRPOEnvTrainer
+from verifiers.utils.data_utils import extract_boxed_answer, extract_hash_answer, preprocess_dataset
 from verifiers.utils.model_utils import get_model, get_tokenizer, get_model_and_tokenizer
-# from verifiers.utils.config_utils import get_default_grpo_config
+from verifiers.utils.config_utils import get_default_grpo_config
 from verifiers.utils.logging_utils import setup_logging, print_prompt_completions_sample
 
 
@@ -17,20 +17,20 @@ __version__ = "0.1.0"
 setup_logging()
 
 __all__ = [
-    # "Environment",
-    # "CodeEnv",
-    # "DoubleCheckEnv",
-    # "MathEnv",
-    # "SimpleEnv",
-    # "ToolEnv",
-    # "GRPOEnvTrainer",
+    "Environment",
+    "CodeEnv",
+    "DoubleCheckEnv",
+    "MathEnv",
+    "SimpleEnv",
+    "ToolEnv",
+    "GRPOEnvTrainer",
     "get_model",
     "get_tokenizer",
     "get_model_and_tokenizer",
-    # "get_default_grpo_config",
-    # "extract_boxed_answer",
-    # "extract_hash_answer",
-    # "preprocess_dataset",
-    # "setup_logging",
-    # "print_prompt_completions_sample",
+    "get_default_grpo_config",
+    "extract_boxed_answer",
+    "extract_hash_answer",
+    "preprocess_dataset",
+    "setup_logging",
+    "print_prompt_completions_sample",
 ]
